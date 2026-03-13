7d843e7a3c089c3d0da6b8e97c1a3658c4c4400e
BautistaLalor <blalor@mail.austral.edu.ar>
2026-03-13 14:19:28 -0300

    Add change calculation functionality
    Implement change calculation based on expense and received money.

--- change.py
+++ change.py
@@ -2,4 +2,23 @@ def change():
     """Lee un gasto y el dinero recibido, calcula el vuelto
     y lo separa en pesos (parte entera) y centavos.
     """
-    pass
+    print("Ingresar gasto:")
+    gasto = float(input())
+    print(gasto)
+
+    print("Dinero recibido")
+    dinero_recibido = int(input())
+    print(dinero_recibido)
+
+    vuelto = dinero_recibido - gasto
+    pesos = int(vuelto)
+
+    centavos = int(vuelto*100 - pesos*100)
+
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print(pesos)
+    print("Centavos:")
+    print(centavos)
