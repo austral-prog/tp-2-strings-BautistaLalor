fe0793b822eb6bd9699036aadfdae2fd3fb01b27
BautistaLalor <blalor@mail.austral.edu.ar>
2026-03-13 14:20:41 -0300

    Add functionality to input and format user names
    Implement user input for first and last name, and print in various formats.

--- names.py
+++ names.py
@@ -2,4 +2,9 @@ def names():
     """Lee nombre y apellido, e imprime el nombre completo en distintos formatos:
     minúsculas, título, mayúsculas y con tabulador.
     """
-    pass
+    nombre_del_usuario = input("Ingresar primer nombre: ")
+    apellido_del_usuario = input("Ingresar apellido: ")
+    print(nombre_del_usuario.lower() + " " + apellido_del_usuario.lower())
+    print(nombre_del_usuario.title() + " " + apellido_del_usuario.title())
+    print(nombre_del_usuario.upper() + " " + apellido_del_usuario.upper())
+    print(f"\t{nombre_del_usuario.lower()} {apellido_del_usuario.lower()}")
